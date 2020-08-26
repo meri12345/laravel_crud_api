93f25d8ea9d2ef27f62e62e4cd6532abca075978
meri12345 <meri_zverka@hotmail.com>
2020-08-26 17:55:52 +0200

    first commit

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PersonResource;
+use App\Http\Resources\PersonResourceCollection;
+use App\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() :PersonResourceCollection
+    {
+       return new PersonResourceCollection(Person::paginate());
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'email'=>'required',
+            'city'=>'required',
+            'phone'=>'required',
+        ]);
+
+        $person = Person::create($request->all());
+
+        return new PersonResource($person);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Person $person):PersonResource
+    {
+        return new PersonResource($person);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Person $person)
+    {
+
+        $person->update($request->all());
+        return new PersonResource($person);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Person $person)
+    {
+        //
+    }
+}
